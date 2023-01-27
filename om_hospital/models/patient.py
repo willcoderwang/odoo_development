56f546d36be6686a00490fbb79720e0cd8b36eb1
Will Wang <wzk@disroot.org>
2023-01-28 09:52:07 +1300

    define models

---
+++ om_hospital/models/patient.py
@@ -0,0 +1,13 @@
+from odoo import models, fields
+
+
+class HospitalPatient(models.Model):
+    _name = "hospital.patient"
+    _description = "Hospital Patient"
+
+    name = fields.Char(string="Name")
+    age = fields.Integer(string="Age")
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('female', 'Female'),
+    ], string="Gender")
