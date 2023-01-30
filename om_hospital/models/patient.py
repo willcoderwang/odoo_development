90b716a069804a7209f9d0bc9c14420f01da3a15
Will Wang <wzk@disroot.org>
2023-01-30 18:52:46 +1300

    define search view

--- om_hospital/models/patient.py
+++ om_hospital/models/patient.py
@@ -6,6 +6,7 @@ class HospitalPatient(models.Model):
     _description = "Hospital Patient"
 
     name = fields.Char(string="Name")
+    ref = fields.Char(string="Reference")
     age = fields.Integer(string="Age")
     gender = fields.Selection([
         ('male', 'Male'),
