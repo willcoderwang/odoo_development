5e13b090f17ca38932f812c123e907f00f165cc5
Will Wang <wzk@disroot.org>
2023-02-11 08:03:44 +1300

    add Date and Datetime

--- om_hospital/models/appointment.py
+++ om_hospital/models/appointment.py
@@ -7,3 +7,5 @@ class HospitalAppointment(models.Model):
     _description = "Hospital Appointment"
 
     patient_id = fields.Many2one(comodel_name='hospital.patient', string="Patient")
+    appointment_time = fields.Datetime()
+    booking_date = fields.Date()
