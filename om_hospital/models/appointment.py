0ca8048372f0218e05df0f65d28b7249b7f2317f
Will Wang <wzk@disroot.org>
2023-02-12 15:49:11 +1300

    add related fields

--- om_hospital/models/appointment.py
+++ om_hospital/models/appointment.py
@@ -7,5 +7,6 @@ class HospitalAppointment(models.Model):
     _description = "Hospital Appointment"
 
     patient_id = fields.Many2one(comodel_name='hospital.patient', string="Patient")
+    gender = fields.Selection(related="patient_id.gender", readonly=False)
     appointment_time = fields.Datetime(default=fields.Datetime.now)
     booking_date = fields.Date(default=fields.Date.context_today)
