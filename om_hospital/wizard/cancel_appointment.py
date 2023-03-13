f5143b5db265d7b6864a5d046140a84f8c9403ed
Will Wang <wzk@disroot.org>
2023-03-13 21:36:02 +1300

    transient model

---
+++ om_hospital/wizard/cancel_appointment.py
@@ -0,0 +1,9 @@
+from odoo import fields, models
+
+
+class CancelAppointmentWizard(models.TransientModel):
+    _name = 'cancel.appointment.wizard'
+    _description = "Cancel Appointment Wizard"
+
+    appointment_id = fields.Many2one('hospital.appointment')
+
