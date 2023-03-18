bd72fc63052dfd079e6dccd7785e50967884e0ff
Will Wang <wzk@disroot.org>
2023-03-18 21:17:16 +1300

    add text field

--- om_hospital/wizard/cancel_appointment.py
+++ om_hospital/wizard/cancel_appointment.py
@@ -6,7 +6,7 @@ class CancelAppointmentWizard(models.TransientModel):
     _description = "Cancel Appointment Wizard"
 
     appointment_id = fields.Many2one('hospital.appointment')
+    reason = fields.Text()
 
     def action_cancel(self):
         return
-
