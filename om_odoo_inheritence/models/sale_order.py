8062b3d586f505d10afa8211df47250f4f5e351c
Will Wang <wzk@disroot.org>
2023-03-25 22:06:31 +1300

    inherit and add field to a model

--- om_odoo_inheritence/models/sale_order.py
+++ om_odoo_inheritence/models/sale_order.py
@@ -4,7 +4,4 @@ from odoo import models, fields
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    name = fields.Char(string="Name", required=True)
-
-    def test_function(self):
-        return
+    confirmed_user_id = fields.Many2one('res.users', string="Confirmed User")
