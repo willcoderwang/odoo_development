94afcca58b05c86d68b7e4260a39d374a5cd94da
Will Wang <wzk@disroot.org>
2023-03-26 13:16:52 +1300

    inherit a function

--- om_odoo_inheritence/models/sale_order.py
+++ om_odoo_inheritence/models/sale_order.py
@@ -5,3 +5,7 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     confirmed_user_id = fields.Many2one('res.users', string="Confirmed User")
+
+    def action_confirm(self):
+        super().action_confirm()
+        self.confirmed_user_id = self.env.user.id
