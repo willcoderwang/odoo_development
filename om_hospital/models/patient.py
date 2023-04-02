c6dff405659742d1f28a985ff33bb5d30a2a03c4
Will Wang <wzk@disroot.org>
2023-04-02 19:21:40 +1200

    python constraints

--- om_hospital/models/patient.py
+++ om_hospital/models/patient.py
@@ -1,6 +1,7 @@
 from datetime import date
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
 
 class HospitalPatient(models.Model):
@@ -19,6 +20,12 @@ class HospitalPatient(models.Model):
     active = fields.Boolean(default=True)
     image = fields.Image()
     tag_ids = fields.Many2many('patient.tag')
+
+    @api.constrains('date_of_birth')
+    def _check_date_of_birth(self):
+        for rec in self:
+            if rec.date_of_birth and rec.date_of_birth > fields.Date.today():
+                raise ValidationError(_("The entered date of birth is not acceptable."))
 
     @api.model_create_multi
     def create(self, vals_list):
