3f2c63fba15c9b2a0a110e3f26af3af695f164db
Will Wang <wzk@disroot.org>
2023-04-02 19:12:44 +1200

    sql constraints

--- om_hospital/models/patient_tag.py
+++ om_hospital/models/patient_tag.py
@@ -9,3 +9,9 @@ class PatientTag(models.Model):
     active = fields.Boolean(default=True)
     color = fields.Integer()
     color_2 = fields.Char()
+    sequence = fields.Integer()
+
+    _sql_constraints = [
+        ('name_uniq', 'unique (name)', "Tag name already exists !"),
+        ('check_sequence', 'check(sequence > 0)', 'The sequence must be a non zero positive number!')
+    ]
