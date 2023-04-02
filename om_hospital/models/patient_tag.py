c06bd9855fefa949bc80801d347299d2b5c6b999
Will Wang <wzk@disroot.org>
2023-04-02 19:38:11 +1200

    copy function

--- om_hospital/models/patient_tag.py
+++ om_hospital/models/patient_tag.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class PatientTag(models.Model):
@@ -7,7 +7,7 @@ class PatientTag(models.Model):
 
     name = fields.Char(required=True)
     active = fields.Boolean(default=True)
-    color = fields.Integer()
+    color = fields.Integer(copy=False)
     color_2 = fields.Char()
     sequence = fields.Integer()
 
@@ -15,3 +15,14 @@ class PatientTag(models.Model):
         ('name_uniq', 'unique (name)', "Tag name already exists !"),
         ('check_sequence', 'check(sequence > 0)', 'The sequence must be a non zero positive number!')
     ]
+
+    @api.returns('self', lambda value: value.id)
+    def copy(self, default=None):
+        if default is None:
+            default = {}
+
+        if not default.get('name'):
+            default['name'] = f"{self.name} (copy)"
+
+        default['sequence'] = 10
+        return super().copy(default)
