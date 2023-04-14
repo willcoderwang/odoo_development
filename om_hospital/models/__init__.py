b91e8259bd1fd833a2177110e3739c62cf1ae6b5
Will Wang <wzk@disroot.org>
2023-04-14 21:18:38 +1200

    add settings

--- om_hospital/models/__init__.py
+++ om_hospital/models/__init__.py
@@ -2,3 +2,4 @@ from . import appointment
 from . import odoo_playground
 from . import patient
 from . import patient_tag
+from . import res_config_settings
---
+++ om_hospital/models/res_config_settings.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = ['res.config.settings']
+
+    cancel_days = fields.Integer(string='Cancel Days', default=5, config_parameter='om_hospital.cancel_days')
