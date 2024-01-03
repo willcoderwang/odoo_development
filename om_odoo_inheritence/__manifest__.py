5fc4dcf29e43543b686ad0e4927d129cd6a25dee
Will Wang <wzk@disroot.org>
2024-01-04 12:41:27 +1300

    fix dependency

--- om_odoo_inheritence/__manifest__.py
+++ om_odoo_inheritence/__manifest__.py
@@ -20,7 +20,10 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['sale'],
+    'depends': [
+        'sale',
+        'stock',
+    ],
 
     # always loaded
     'data': [
